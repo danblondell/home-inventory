8728b9daab954c4de642892ee764df757e6238cf
Dan Blondell <danblondell@gmail.com>
2018-01-10 00:11:56 -0500

    Adjust indentation in migration files

--- db/migrate/20180110024047_create_rooms.rb
+++ db/migrate/20180110024047_create_rooms.rb
@@ -1,9 +1,9 @@
 class CreateRooms < ActiveRecord::Migration[5.1]
-  def change
-  	create_table :rooms do |t|
-  		t.string :name
-  		t.text :notes
-  		t.integer :owner_id
-  	end
-  end
+	def change
+		create_table :rooms do |t|
+			t.string :name
+			t.text :notes
+			t.integer :owner_id
+		end
+	end
 end
--- db/migrate/20180110043043_change_rooms.rb
+++ db/migrate/20180110043043_change_rooms.rb
@@ -1,5 +1,5 @@
 class ChangeRooms < ActiveRecord::Migration[5.1]
-  def change
-  	rename_column(:rooms, :owner_id, :user_id)
-  end
+	def change
+		rename_column(:rooms, :owner_id, :user_id)
+	end
 end
