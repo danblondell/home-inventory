0a9cf589ea508fb9d78d8fccd3b7bf69a1ee74a8
Dan Blondell <danblondell@gmail.com>
2018-01-09 22:29:22 -0500

    Add user signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
 
 
 	get '/users/signup' do
+		erb :'/users/signup'
+	end
+
+	post '/users' do
+		@user = User.new(username: params[:username], first_name: params[:first_name], email: params[:email], password: params[:password])
+		
 		
 	end
 
 	get '/users/login' do
-		
+		erb :'/users/login'
 	end
 
 
