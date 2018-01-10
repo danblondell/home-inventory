919a639b328308b8de4115d03c0cb446a922e808
Dan Blondell <danblondell@gmail.com>
2018-01-09 23:32:38 -0500

    Fix name of user_id from erroneous owner_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110024059) do
+ActiveRecord::Schema.define(version: 20180110043043) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180110024059) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.text "notes"
-    t.integer "owner_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
