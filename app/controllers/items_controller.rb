341c65da9bc5a2cfad330196a7b3d5e860309568
Dan Blondell <danblondell@gmail.com>
2018-01-11 18:59:49 -0500

    Start new items form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,24 @@ class ItemsController < ApplicationController
 		erb :'/items/index'
 	end
 
+	get '/items/new' do
+		redirect_to_login_page_if_not_logged_in
+
+		@user = current_user
+		erb :'/items/create_item'
+	end
+
+	post '/items/new' do
+		if item = current_user.items.find_by(name: params[:item][:name])
+			redirect "/items/#{item.id}"
+		end
+
+		item = Room.new(params[:item])
+		current_user.items << item
+
+		item.save
+
+		redirect "/items/#{item.id}"
+	end
+
 end
